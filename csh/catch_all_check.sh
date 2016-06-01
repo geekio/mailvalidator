8d522dd1b773cea9ece65fde70d84e0ad7bfb119
Charlie Root <root@mail.iomatters.com>
2016-06-01 15:13:30 +0000

    ptr and sender address added in the mailvalidator call

--- csh/catch_all_check.sh
+++ csh/catch_all_check.sh
@@ -1,3 +1,3 @@
 EMAIL=$1
-RETURN=$(/usr/local/mailvalidator/mailvalidator -catchall $EMAIL | awk '{print $2}')
+RETURN=$(/usr/local/mailvalidator/mailvalidator -p mail.iomatters.com -s admin@iomatters.com -catchall $EMAIL | awk '{print $2}')
 echo $RETURN;
